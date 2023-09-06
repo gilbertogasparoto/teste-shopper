670ba239bf11cb95d1c69839d3dcade8a85040ec
unknown <gilbertohfgasparoto@gmail.com>
2023-09-05 23:19:32 -0300

    Criando estrutura da página principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Papa from "papaparse";
+import "./styles.css";
+import { UploadIcon } from "./assets/icons/upload-icon";
 
 function App() {
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target && e.target.files && e.target.files.length) {
+      Papa.parse(e.target.files[0], {
+        header: true,
+        skipEmptyLines: true,
+        complete: function (results: any) {
+          console.log(results.data);
+        },
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="modal-upload">
+      <header>
+        <h2>Carregar CSV</h2>
       </header>
+
+      <main>
+        <label className="file-uploader" htmlFor="uploader">
+          <UploadIcon />
+          Selecione o arquivo
+        </label>
+        <input
+          id="uploader"
+          type="file"
+          name="file"
+          accept=".csv"
+          onChange={changeHandler}
+        />
+      </main>
+
+      <footer>
+        <button>Validar</button>
+      </footer>
     </div>
   );
 }
